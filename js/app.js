813ec9f77e01b4d1a987ebb983118100e9ef528e
Mohamed Nuh <mojitsu185@gmail.com>
2020-04-12 22:49:16 -0700

    Grabbed all the elements

---
+++ js/app.js
@@ -0,0 +1,8 @@
+// Grabbing all the required elements
+
+const formMain = document.getElementsByTagName("form")[0];
+const formParent = document.getElementsByClassName("container")[0];
+const nameField = document.getElementById("name");
+const emailField = document.getElementById("mail");
+const jobRole = document.getElementById("title");
+const shirt = document.getElementsByClassName("shirt")[0];
