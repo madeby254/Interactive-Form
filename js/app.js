ab9ae10fa58b0a3354673b48dc0de27c69af0de3
Mohamed Nuh <mojitsu185@gmail.com>
2020-04-12 22:55:43 -0700

    Added some more js

--- js/app.js
+++ js/app.js
@@ -5,4 +5,17 @@ const formParent = document.getElementsByClassName("container")[0];
 const nameField = document.getElementById("name");
 const emailField = document.getElementById("mail");
 const jobRole = document.getElementById("title");
+const OtherRoles = document.getElementById("other_basic_info");
 const shirt = document.getElementsByClassName("shirt")[0];
+
+// Drop Down selection if other is selected
+
+OtherRoles.style.display = 'none'
+
+jobRole.addEventListener('change', (e) => {
+    if (e.target.value.toLowerCase () === "other") {
+        OtherRoles.style.display =''
+    } else {
+        OtherRoles.style.display = 'none'
+    }
+})
