a5051f08f2c19c1660225dfa64666ca81cb6aedd
Mohamed Nuh <mojitsu185@gmail.com>
2020-04-13 17:33:54 -0700

    main form

---
+++ js/app.js
@@ -0,0 +1,10 @@
+
+
+// Const for stroing form details
+
+const form = document.querySelector("form");
+const nameField = document.querySelector("#name");
+const emailField = document.querySelector("#mail");
+const paymentField = document.querySelector("#cc-num");
+const zipcodeField = document.querySelector("#zip");
+const cvv = document.querySelector("#cvv");
