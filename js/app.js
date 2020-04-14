ad88a6a8629a04966c40ed73ddd815865d13bffb
Mohamed Nuh <mojitsu185@gmail.com>
2020-04-13 21:42:56 -0700

    Grabbed more const

---
+++ js/app.js
@@ -0,0 +1,20 @@
+// Created a const to store the Form Fields
+
+const formField = document.querySelector("form");
+const nameField = document.querySelector("#name");
+const emailField = document.querySelector("#mail");
+const creditcardField = document.querySelector("#cc-num");
+const zipcodeField = document.querySelector("#zip");
+const cvv = document.querySelector("#cvv");
+
+// Const for the dropdown menu
+
+const role = document.getElementById("title")
+const otherRole = document.getElementById("other-title")
+
+// Const for the Deisign and Color tabs on the T-shirt Info
+
+const designTab = ocument.getElementById('design')
+
+
+
