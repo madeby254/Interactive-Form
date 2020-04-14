d1c4387dd418d29c41961ffb050d67a9608053d0
Mohamed Nuh <mojitsu185@gmail.com>
2020-04-13 22:00:40 -0700

    Added theme

--- js/app.js
+++ js/app.js
@@ -14,13 +14,13 @@ const otherRole = document.getElementById("other-title")
 
 // Const for the Deisign and Color tabs on the T-shirt Info
 
-const designTab = ocument.getElementById('design')
+const designTab = document.getElementById('design')
 const colorTab = document.getElementById('color')
 
 // Const for Register for Activities tab
 
 const activities = document.getElementsByClassName("activities")
-const checkboxes = $('.activities input:checkbox')
+const checkboxes = `$('.activities input:checkbox')`
 let price = 0;
 const totalPrice = document.createElement('div')
 totalPrice.className = 'total-price'
@@ -35,3 +35,23 @@ role.addEventListener ('change', (event) => {
         otherRole.style.display = 'none'
     }
 })
+
+// Hides all the other options
+
+function hideColors() {
+    $('#color').children().hide();
+}
+
+// This displays the select a theme option in the color dropdown
+
+function selectTheme() {
+    let option = document.createElement("option");
+    option.value = 'selecttheme';
+    option.text = 'Choose on theme to proceed';
+    colorSelection.appendChild(option);
+    option.setAttribute('selected', 'selected');
+    }
+
+    // Called both functions here 
+    hideColors()
+    selectTheme()
