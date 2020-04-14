e56475ce44795a732f6f6d778755252d0376ed7c
Mohamed Nuh <mojitsu185@gmail.com>
2020-04-13 21:53:13 -0700

    Other Options

--- js/app.js
+++ js/app.js
@@ -15,6 +15,23 @@ const otherRole = document.getElementById("other-title")
 // Const for the Deisign and Color tabs on the T-shirt Info
 
 const designTab = ocument.getElementById('design')
+const colorTab = document.getElementById('color')
 
+// Const for Register for Activities tab
 
+const activities = document.getElementsByClassName("activities")
+const checkboxes = $('.activities input:checkbox')
+let price = 0;
+const totalPrice = document.createElement('div')
+totalPrice.className = 'total-price'
+let content = totalPrice.textContent = 'Your Total Price: $' + totalPrice + '.00' // Make this object lit
 
+// Shows other title when the other option is selected
+
+role.addEventListener ('change', (event) => {
+    if (event.target.value = 'other') {
+        otherRole.style.display = 'block';
+    } else {
+        otherRole.style.display = 'none'
+    }
+})
